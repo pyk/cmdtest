ee268ffdcc73cdaf4412d81f6505050fb71827fa
pyk <2213646+pyk@users.noreply.github.com>
2025-11-06 22:55:42 +0700

    fix: try to move `add` to `build.zig`

--- build.zig
+++ build.zig
@@ -1,9 +1,61 @@
 const std = @import("std");
 const fs = std.fs;
 const mem = std.mem;
-const exetest = @import("./src/root.zig");
+const Build = std.Build;
 
-pub fn build(b: *std.Build) !void {
+pub const AddOptions = struct {
+    /// Name of the test target
+    name: []const u8,
+    /// Path to the test source file
+    test_file: Build.LazyPath,
+    /// The `exetest` build module to import into the test
+    exetest_mod: ?*Build.Module,
+};
+
+/// Register new test
+pub fn add(b: *Build, options: AddOptions) *Build.Step.Run {
+    const exetest_mod = if (options.exetest_mod) |mod|
+        mod
+    else
+        b.dependency("exetest", .{
+            .target = b.graph.host,
+        }).module("exetest");
+
+    // Create the test module that imports the runtime module
+    const test_mod = b.createModule(.{
+        .root_source_file = options.test_file,
+        .target = b.graph.host,
+        .imports = &.{
+            .{
+                .name = "exetest",
+                .module = exetest_mod,
+            },
+        },
+    });
+
+    // Create the test executable compilation step
+    const test_exe = b.addTest(.{
+        .name = options.name,
+        .root_module = test_mod,
+    });
+    const run_test_exe = b.addRunArtifact(test_exe);
+
+    // IMPORTANT: Make sure all exe are installed first
+    run_test_exe.step.dependOn(b.getInstallStep());
+
+    const original_path = b.graph.env_map.get("PATH") orelse "";
+    const path = b.fmt("{s}{c}{s}", .{
+        b.exe_dir,
+        std.fs.path.delimiter,
+        original_path,
+    });
+
+    run_test_exe.setEnvironmentVariable("PATH", path);
+
+    return run_test_exe;
+}
+
+pub fn build(b: *Build) !void {
     const target = b.standardTargetOptions(.{});
 
     ////////////////////////////////////////////////////////////////
@@ -35,7 +87,7 @@ pub fn build(b: *std.Build) !void {
     });
     b.installArtifact(test_exe);
 
-    const run_integration_tests = exetest.add(b, .{
+    const run_integration_tests = add(b, .{
         .name = "integration",
         .test_file = b.path("src/test.zig"),
         .exetest_mod = mod,
--- src/root.zig
+++ src/root.zig
@@ -1,59 +1,6 @@
 const std = @import("std");
-const Build = std.Build;
 const testing = std.testing;
 const Child = std.process.Child;
-
-pub const AddOptions = struct {
-    /// Name of the test target
-    name: []const u8,
-    /// Path to the test source file
-    test_file: Build.LazyPath,
-    /// The `exetest` build module to import into the test
-    exetest_mod: ?*Build.Module,
-};
-
-/// Register new test
-pub fn add(b: *Build, options: AddOptions) *Build.Step.Run {
-    const exetest_mod = if (options.exetest_mod) |mod|
-        mod
-    else
-        b.dependency("exetest", .{
-            .target = b.graph.host,
-        }).module("exetest");
-
-    // Create the test module that imports the runtime module
-    const test_mod = b.createModule(.{
-        .root_source_file = options.test_file,
-        .target = b.graph.host,
-        .imports = &.{
-            .{
-                .name = "exetest",
-                .module = exetest_mod,
-            },
-        },
-    });
-
-    // Create the test executable compilation step
-    const test_exe = b.addTest(.{
-        .name = options.name,
-        .root_module = test_mod,
-    });
-    const run_test_exe = b.addRunArtifact(test_exe);
-
-    // IMPORTANT: Make sure all exe are installed first
-    run_test_exe.step.dependOn(b.getInstallStep());
-
-    const original_path = b.graph.env_map.get("PATH") orelse "";
-    const path = b.fmt("{s}{c}{s}", .{
-        b.exe_dir,
-        std.fs.path.delimiter,
-        original_path,
-    });
-
-    run_test_exe.setEnvironmentVariable("PATH", path);
-
-    return run_test_exe;
-}
 
 /// Options for `run` controlling I/O, allocator, and output limits.
 pub const RunOptions = struct {
